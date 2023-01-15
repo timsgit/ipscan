dbfd5be7e9d62f3b835cd754d9a96a441fb9008f
Tim Chappell <webmaster@chappell-family.com>
2023-01-15 16:35:55 +0000

    correct format strings

--- ipscan_web.c
+++ ipscan_web.c
@@ -378,7 +378,7 @@ void create_html_header(uint16_t numports, uint16_t numudpports, char * reconque
 	printf(" psp = latestState[i];");
 	// >>> is unsigned shift right
 	printf(" proto = ((psp >>> %d) & %u);", IPSCAN_PROTO_SHIFT, IPSCAN_PROTO_MASK);
-	printf(" special = ((psp >>> %d) & %u);", IPSCAN_SPECIAL_SHIFT, IPSCAN_SPECIAL_MASK);
+	printf(" special = ((psp >>> %u) & %u);", IPSCAN_SPECIAL_SHIFT, IPSCAN_SPECIAL_MASK);
 	printf(" port = ((psp >>> %d) & %u);", IPSCAN_PORT_SHIFT, IPSCAN_PORT_MASK);
 	printf(" result = latestState[i+1];");
 	printf(" host = latestState[i+2];");
